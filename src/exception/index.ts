03164b7a4fc6665343dd6733419f505a004106b1
bingtsingw <10382462+bingtsingw@users.noreply.github.com>
2023-06-20 11:16:58 +0800

    fix: export exception.BaseException

--- src/exception/index.ts
+++ src/exception/index.ts
@@ -4,8 +4,10 @@ import { ForbiddenException } from './403';
 import { NotFoundException } from './404';
 import { ClientException } from './4xx';
 import { ServerException } from './5xx';
+import { BaseException } from './base';
 
 export const exception = {
+  BaseException,
   ClientException,
   BadRequestException,
   UnauthorizedException,
