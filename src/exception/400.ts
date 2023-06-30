ca995f42d2a4c90f17451a2a5825c74e66fccb3e
bingtsingw <10382462+bingtsingw@users.noreply.github.com>
2023-06-30 17:54:13 +0800

    fix: exception accept optional messages

--- src/exception/400.ts
+++ src/exception/400.ts
@@ -1,7 +1,7 @@
 import { BaseException } from './base';
 
 export class BadRequestException extends BaseException {
-  public constructor(messages: unknown, code?: string) {
+  public constructor(messages?: unknown, code?: string) {
     super(400, messages, code);
   }
 }
--- src/exception/403.ts
+++ src/exception/403.ts
@@ -1,7 +1,7 @@
 import { BaseException } from './base';
 
 export class ForbiddenException extends BaseException {
-  public constructor(messages: unknown, code?: string) {
+  public constructor(messages?: unknown, code?: string) {
     super(403, messages, code);
   }
 }
