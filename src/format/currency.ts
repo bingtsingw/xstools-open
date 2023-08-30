fee1f26a60d7ee5499a9c141b2b8566dddde65c6
bingtsingw <10382462+bingtsingw@users.noreply.github.com>
2023-08-30 15:06:28 +0800

    fix: update format.currency

--- src/format/currency.ts
+++ src/format/currency.ts
@@ -1,7 +1,21 @@
-export const currency = (currency: number, options?: { decimals: number; symbol: string; F2Y: boolean }) => {
-  const { decimals = 2, symbol = '¥' } = options || {};
+export const currency = (
+  currency: number,
+  options?: { decimals?: number; symbol?: string; sign?: boolean; F2Y?: boolean },
+) => {
+  const { decimals = 2, symbol = '¥', sign = false } = options || {};
 
   const dm = decimals < 0 ? 0 : decimals;
 
-  return symbol + ' ' + (currency / 100).toFixed(dm);
+  let num = (currency / 100).toFixed(dm);
+
+  if (sign) {
+    const _sign = currency > 0 ? '+' : '';
+    num = _sign + num;
+  }
+
+  if (symbol) {
+    num = symbol + ' ' + num;
+  }
+
+  return num;
 };
