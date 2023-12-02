b640bbd260e46183bdb14a3ff839e9a0eaff36af
bingtsingw <10382462+bingtsingw@users.noreply.github.com>
2023-12-02 21:02:26 +0800

    refactor: migrate dayjs to date-fns

--- src/datetime/cn-week-day.ts
+++ src/datetime/cn-week-day.ts
@@ -1,23 +1,22 @@
-import dayjs from 'dayjs';
-import { _exception } from '../exception';
+import { getDay, parseISO } from 'date-fns';
 
-export const cnWeekDay = (date: string): string => {
-  switch (dayjs(date).format('d')) {
-    case '0':
+type CnWeekDay = '周日' | '周一' | '周二' | '周三' | '周四' | '周五' | '周六';
+
+export const cnWeekDay = (date: string): CnWeekDay => {
+  switch (getDay(parseISO(date))) {
+    case 0:
       return '周日';
-    case '1':
+    case 1:
       return '周一';
-    case '2':
+    case 2:
       return '周二';
-    case '3':
+    case 3:
       return '周三';
-    case '4':
+    case 4:
       return '周四';
-    case '5':
+    case 5:
       return '周五';
-    case '6':
+    case 6:
       return '周六';
-    default:
-      throw new _exception.server.InternalErrorException('unexpected date');
   }
 };
