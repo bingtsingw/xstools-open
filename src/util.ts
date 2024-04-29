c95b7813bc584d7b3e259f9d7224283ea4b0f331
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2024-04-29 19:54:32 +0800

    feat: 重构whitelist默认不传的逻辑

--- src/util.ts
+++ src/util.ts
@@ -7,22 +7,36 @@ export const getDistrictFromAddress = ({
   address: string;
   cityWhiteList?: string[];
 }): string => {
-  if (cityWhiteList && Array.isArray(cityWhiteList) && cityWhiteList.length > 0) {
-    const regex =
-      /(?<province>[^省]+省|.+自治区|[^澳门]+澳门|[^香港]+香港|[^市]+市)?(?<city>[^自治州]+自治州|[^特别行政区]+特别行政区|[^市]+市|.*?地区|.*?行政单位|.+盟|市辖区|[^县]+县)(?<district>[^县]+县|[^市]+市|[^镇]+镇|[^区]+区|[^乡]+乡|.+场|.+旗|.+海域|.+岛)?(?<address>.*)/;
-    const match = address.match(regex);
-    if (match) {
-      const { province, city, district } = match.groups as {
-        province: string;
-        city: string;
-        district: string;
-      };
-      if (cityWhiteList.some((c) => province?.includes(c)) || cityWhiteList.some((c) => city?.includes(c))) {
-        if (district && !title.includes(district) && !title.includes(district.replace('区', ''))) {
-          return district;
-        }
-      }
+  const regex =
+    /(?<province>[^省]+省|.+自治区|[^澳门]+澳门|[^香港]+香港|[^市]+市)?(?<city>[^自治州]+自治州|[^特别行政区]+特别行政区|[^市]+市|.*?地区|.*?行政单位|.+盟|市辖区|[^县]+县)(?<district>[^县]+县|[^市]+市|[^镇]+镇|[^区]+区|[^乡]+乡|.+场|.+旗|.+海域|.+岛)?(?<address>.*)/;
+  const match: any = address.match(regex);
+  if (match) {
+    const { district } = match.groups as {
+      district: string;
+    };
+    if (isValidDistrict({ title, district, cityWhiteList, locationGroups: match.groups })) {
+      return district;
     }
   }
+
   return '';
 };
+
+function isValidDistrict({
+  title,
+  district,
+  cityWhiteList,
+  locationGroups,
+}: {
+  title: string;
+  district: string;
+  cityWhiteList: string[] | undefined;
+  locationGroups: { province?: string; city?: string };
+}): boolean {
+  if (!district) return false;
+  if (title.includes(district) || title.includes(district.replace('区', ''))) return false;
+  if (!cityWhiteList || !cityWhiteList.length) return true;
+
+  const { province, city } = locationGroups;
+  return cityWhiteList.some((c) => province?.includes(c) || city?.includes(c));
+}
