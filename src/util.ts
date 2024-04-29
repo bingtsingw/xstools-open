89c3df156932d4aa70938355206bacc9aa3d5655
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2024-04-29 23:32:24 +0800

    fix: small fix

--- src/util.ts
+++ src/util.ts
@@ -4,9 +4,11 @@ export const getDistrictFromAddress = ({
   cityWhiteList,
 }: {
   title: string;
-  address: string;
+  address?: string;
   cityWhiteList?: string[];
 }): string => {
+  if (!address) return '';
+
   const regex =
     /(?<province>[^省]+省|.+自治区|[^澳门]+澳门|[^香港]+香港|[^市]+市)?(?<city>[^自治州]+自治州|[^特别行政区]+特别行政区|[^市]+市|.*?地区|.*?行政单位|.+盟|市辖区|[^县]+县)(?<district>[^县]+县|[^市]+市|[^镇]+镇|[^区]+区|[^乡]+乡|.+场|.+旗|.+海域|.+岛)?(?<address>.*)/;
   const match: any = address.match(regex);
