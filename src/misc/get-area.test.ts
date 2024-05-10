580c567fd46db6bc197699c8a0d33b1fc0ec49ef
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2024-05-10 21:43:42 +0800

    fix: small fix

--- src/misc/get-area.test.ts
+++ src/misc/get-area.test.ts
@@ -35,300 +35,300 @@ describe('getArea', () => {
       });
       expect(area).toBe(address.area);
     });
-    test('合法地址', () => {
-      [
-        {
-          address: '北京市北京市大兴区行远街道101号',
-          title: '星巴克',
-          cityWhiteList: ['北京市'],
-          expected: '大兴区',
-        },
-        {
-          address: '北京市西城区复兴门内大街101号',
-          title: '百盛购物中心(复兴门店)',
-          cityWhiteList: ['北京市'],
-          expected: '西城区',
-        },
-        {
-          address: '北京市东城区安定门外大街',
-          title: '地坛公园',
-          cityWhiteList: ['北京市'],
-          expected: '东城区',
-        },
-        {
-          address: '上海市普陀区曹杨路1040弄1-2号',
-          title: '伟龙商务楼',
-          cityWhiteList: ['上海市'],
-          expected: '普陀区',
-        },
-        {
-          address: '上海市静安区梅园路360号',
-          title: '环龙商场',
-          cityWhiteList: ['上海市'],
-          expected: '静安区',
-        },
-        {
-          address: '上海市黄浦区淮海中路282~283号',
-          title: '上海香港广场',
-          cityWhiteList: ['上海市'],
-          expected: '黄浦区',
-        },
-        {
-          address: '重庆市渝北区洪湖东路1号',
-          title: '财富MALL',
-          cityWhiteList: ['重庆市'],
-          expected: '渝北区',
-        },
-        {
-          address: '重庆市北部新区泰山大道东段62号',
-          title: '动力时光购物中心',
-          cityWhiteList: ['重庆市'],
-          expected: '北部新区',
-        },
-        {
-          address: '重庆市江北区观音桥步行街6号',
-          title: '重庆新世界百货',
-          cityWhiteList: ['重庆市'],
-          expected: '江北区',
-        },
-        {
-          address: '广东省深圳市福田区莲花街道红荔路6030号',
-          title: '莲花山公园',
-          cityWhiteList: ['深圳市'],
-          expected: '福田区',
-        },
-        {
-          address: '广东省深圳市罗湖区解放路2001号',
-          title: '太阳百货',
-          cityWhiteList: ['广东省'],
-          expected: '罗湖区',
-        },
-        {
-          address: '广东省深圳市罗湖区宝安南路1881号',
-          title: '深圳万象城',
-          cityWhiteList: ['广东省'],
-          expected: '罗湖区',
-        },
-        // 无白名单
-        {
-          address: '北京市北京市大兴区行远街道101号',
-          title: '星巴克',
-          expected: '大兴区',
-        },
-        {
-          address: '北京市西城区复兴门内大街101号',
-          title: '百盛购物中心(复兴门店)',
-          expected: '西城区',
-        },
-        {
-          address: '北京市东城区安定门外大街',
-          title: '地坛公园',
-          expected: '东城区',
-        },
-        {
-          address: '上海市普陀区曹杨路1040弄1-2号',
-          title: '伟龙商务楼',
-          expected: '普陀区',
-        },
-        {
-          address: '上海市静安区梅园路360号',
-          title: '环龙商场',
-          expected: '静安区',
-        },
-        {
-          address: '上海市黄浦区淮海中路282~283号',
-          title: '上海香港广场',
-          expected: '黄浦区',
-        },
-        {
-          address: '重庆市渝北区洪湖东路1号',
-          title: '财富MALL',
-          expected: '渝北区',
-        },
-        {
-          address: '重庆市北部新区泰山大道东段62号',
-          title: '动力时光购物中心',
-          expected: '北部新区',
-        },
-        {
-          address: '重庆市江北区观音桥步行街6号',
-          title: '重庆新世界百货',
-          expected: '江北区',
-        },
-        {
-          address: '广东省深圳市福田区莲花街道红荔路6030号',
-          title: '莲花山公园',
-          expected: '福田区',
-        },
-        {
-          address: '广东省深圳市罗湖区解放路2001号',
-          title: '太阳百货',
-          expected: '罗湖区',
-        },
-        {
-          address: '广东省深圳市罗湖区宝安南路1881号',
-          title: '深圳万象城',
-          expected: '罗湖区',
-        },
-      ].forEach(({ address, title, cityWhiteList, expected }) => {
-        const area = getArea({ address, title, cityWhiteList });
-        expect(area).toBe(expected);
-      });
+  });
+  test('合法地址', () => {
+    [
+      {
+        address: '北京市北京市大兴区行远街道101号',
+        title: '星巴克',
+        cityWhiteList: ['北京市'],
+        expected: '大兴区',
+      },
+      {
+        address: '北京市西城区复兴门内大街101号',
+        title: '百盛购物中心(复兴门店)',
+        cityWhiteList: ['北京市'],
+        expected: '西城区',
+      },
+      {
+        address: '北京市东城区安定门外大街',
+        title: '地坛公园',
+        cityWhiteList: ['北京市'],
+        expected: '东城区',
+      },
+      {
+        address: '上海市普陀区曹杨路1040弄1-2号',
+        title: '伟龙商务楼',
+        cityWhiteList: ['上海市'],
+        expected: '普陀区',
+      },
+      {
+        address: '上海市静安区梅园路360号',
+        title: '环龙商场',
+        cityWhiteList: ['上海市'],
+        expected: '静安区',
+      },
+      {
+        address: '上海市黄浦区淮海中路282~283号',
+        title: '上海香港广场',
+        cityWhiteList: ['上海市'],
+        expected: '黄浦区',
+      },
+      {
+        address: '重庆市渝北区洪湖东路1号',
+        title: '财富MALL',
+        cityWhiteList: ['重庆市'],
+        expected: '渝北区',
+      },
+      {
+        address: '重庆市北部新区泰山大道东段62号',
+        title: '动力时光购物中心',
+        cityWhiteList: ['重庆市'],
+        expected: '北部新区',
+      },
+      {
+        address: '重庆市江北区观音桥步行街6号',
+        title: '重庆新世界百货',
+        cityWhiteList: ['重庆市'],
+        expected: '江北区',
+      },
+      {
+        address: '广东省深圳市福田区莲花街道红荔路6030号',
+        title: '莲花山公园',
+        cityWhiteList: ['深圳市'],
+        expected: '福田区',
+      },
+      {
+        address: '广东省深圳市罗湖区解放路2001号',
+        title: '太阳百货',
+        cityWhiteList: ['广东省'],
+        expected: '罗湖区',
+      },
+      {
+        address: '广东省深圳市罗湖区宝安南路1881号',
+        title: '深圳万象城',
+        cityWhiteList: ['广东省'],
+        expected: '罗湖区',
+      },
+      // 无白名单
+      {
+        address: '北京市北京市大兴区行远街道101号',
+        title: '星巴克',
+        expected: '大兴区',
+      },
+      {
+        address: '北京市西城区复兴门内大街101号',
+        title: '百盛购物中心(复兴门店)',
+        expected: '西城区',
+      },
+      {
+        address: '北京市东城区安定门外大街',
+        title: '地坛公园',
+        expected: '东城区',
+      },
+      {
+        address: '上海市普陀区曹杨路1040弄1-2号',
+        title: '伟龙商务楼',
+        expected: '普陀区',
+      },
+      {
+        address: '上海市静安区梅园路360号',
+        title: '环龙商场',
+        expected: '静安区',
+      },
+      {
+        address: '上海市黄浦区淮海中路282~283号',
+        title: '上海香港广场',
+        expected: '黄浦区',
+      },
+      {
+        address: '重庆市渝北区洪湖东路1号',
+        title: '财富MALL',
+        expected: '渝北区',
+      },
+      {
+        address: '重庆市北部新区泰山大道东段62号',
+        title: '动力时光购物中心',
+        expected: '北部新区',
+      },
+      {
+        address: '重庆市江北区观音桥步行街6号',
+        title: '重庆新世界百货',
+        expected: '江北区',
+      },
+      {
+        address: '广东省深圳市福田区莲花街道红荔路6030号',
+        title: '莲花山公园',
+        expected: '福田区',
+      },
+      {
+        address: '广东省深圳市罗湖区解放路2001号',
+        title: '太阳百货',
+        expected: '罗湖区',
+      },
+      {
+        address: '广东省深圳市罗湖区宝安南路1881号',
+        title: '深圳万象城',
+        expected: '罗湖区',
+      },
+    ].forEach(({ address, title, cityWhiteList, expected }) => {
+      const area = getArea({ address, title, cityWhiteList });
+      expect(area).toBe(expected);
     });
+  });
 
-    test('合法地址：title为空', () => {
-      [
-        {
-          address: '北京市北京市大兴区行远街道101号',
-          title: '',
-          cityWhiteList: ['北京市'],
-          expected: '大兴区',
-        },
-        {
-          address: '北京市西城区复兴门内大街101号',
-          title: '',
-          cityWhiteList: ['北京市'],
-          expected: '西城区',
-        },
-      ].forEach(({ address, title, cityWhiteList, expected }) => {
-        const area = getArea({ address, title, cityWhiteList });
-        expect(area).toBe(expected);
-      });
+  test('合法地址：title为空', () => {
+    [
+      {
+        address: '北京市北京市大兴区行远街道101号',
+        title: '',
+        cityWhiteList: ['北京市'],
+        expected: '大兴区',
+      },
+      {
+        address: '北京市西城区复兴门内大街101号',
+        title: '',
+        cityWhiteList: ['北京市'],
+        expected: '西城区',
+      },
+    ].forEach(({ address, title, cityWhiteList, expected }) => {
+      const area = getArea({ address, title, cityWhiteList });
+      expect(area).toBe(expected);
     });
+  });
 
-    test('非法地址：不再白名单（return ""）', () => {
-      [
-        {
-          address: '北京市北京市大兴区行远街道101号',
-          title: '星巴克',
-          cityWhiteList: ['上海市'],
-          expected: '',
-        },
-        {
-          address: '上海市普陀区曹杨路1040弄1-2号',
-          title: '伟龙商务楼',
-          cityWhiteList: ['北京市'],
-          expected: '',
-        },
-        {
-          address: '重庆市渝北区洪湖东路1号',
-          title: '财富MALL',
-          cityWhiteList: ['上海市'],
-          expected: '',
-        },
-        {
-          address: '广东省深圳市福田区莲花街道红荔路6030号',
-          title: '莲花山公园',
-          cityWhiteList: ['北京市'],
-          expected: '',
-        },
-      ].forEach(({ address, title, cityWhiteList, expected }) => {
-        const area = getArea({ address, title, cityWhiteList });
-        expect(area).toBe(expected);
-      });
+  test('非法地址：不再白名单（return ""）', () => {
+    [
+      {
+        address: '北京市北京市大兴区行远街道101号',
+        title: '星巴克',
+        cityWhiteList: ['上海市'],
+        expected: '',
+      },
+      {
+        address: '上海市普陀区曹杨路1040弄1-2号',
+        title: '伟龙商务楼',
+        cityWhiteList: ['北京市'],
+        expected: '',
+      },
+      {
+        address: '重庆市渝北区洪湖东路1号',
+        title: '财富MALL',
+        cityWhiteList: ['上海市'],
+        expected: '',
+      },
+      {
+        address: '广东省深圳市福田区莲花街道红荔路6030号',
+        title: '莲花山公园',
+        cityWhiteList: ['北京市'],
+        expected: '',
+      },
+    ].forEach(({ address, title, cityWhiteList, expected }) => {
+      const area = getArea({ address, title, cityWhiteList });
+      expect(area).toBe(expected);
     });
-    test('非法地址：title包含区（return ""）', () => {
-      [
-        {
-          address: '北京市北京市大兴区行远街道101号',
-          title: '大兴星巴克',
-          cityWhiteList: ['北京市'],
-          expected: '',
-        },
-        {
-          address: '上海市普陀区曹杨路1040弄1-2号',
-          title: '(普陀)伟龙商务楼',
-          cityWhiteList: ['上海市'],
-          expected: '',
-        },
-        {
-          address: '重庆市渝北区洪湖东路1号',
-          title: '(渝北区)财富MALL',
-          cityWhiteList: ['重庆市'],
-          expected: '',
-        },
-        {
-          address: '广东省深圳市福田区莲花街道红荔路6030号',
-          title: '(福田区)莲花山公园',
-          cityWhiteList: ['广东省'],
-          expected: '',
-        },
-        {
-          address: '广东省深圳市福田区莲花街道红荔路6030号',
-          title: '莲花山公园福田',
-          cityWhiteList: ['广东省'],
-          expected: '',
-        },
-        {
-          address: '广东省深圳市福田区莲花街道红荔路6030号',
-          title: '莲花山公园【福田】',
-          cityWhiteList: ['广东省'],
-          expected: '',
-        },
-      ].forEach(({ address, title, cityWhiteList, expected }) => {
-        const area = getArea({ address, title, cityWhiteList });
-        expect(area).toBe(expected);
-      });
+  });
+  test('非法地址：title包含区（return ""）', () => {
+    [
+      {
+        address: '北京市北京市大兴区行远街道101号',
+        title: '大兴星巴克',
+        cityWhiteList: ['北京市'],
+        expected: '',
+      },
+      {
+        address: '上海市普陀区曹杨路1040弄1-2号',
+        title: '(普陀)伟龙商务楼',
+        cityWhiteList: ['上海市'],
+        expected: '',
+      },
+      {
+        address: '重庆市渝北区洪湖东路1号',
+        title: '(渝北区)财富MALL',
+        cityWhiteList: ['重庆市'],
+        expected: '',
+      },
+      {
+        address: '广东省深圳市福田区莲花街道红荔路6030号',
+        title: '(福田区)莲花山公园',
+        cityWhiteList: ['广东省'],
+        expected: '',
+      },
+      {
+        address: '广东省深圳市福田区莲花街道红荔路6030号',
+        title: '莲花山公园福田',
+        cityWhiteList: ['广东省'],
+        expected: '',
+      },
+      {
+        address: '广东省深圳市福田区莲花街道红荔路6030号',
+        title: '莲花山公园【福田】',
+        cityWhiteList: ['广东省'],
+        expected: '',
+      },
+    ].forEach(({ address, title, cityWhiteList, expected }) => {
+      const area = getArea({ address, title, cityWhiteList });
+      expect(area).toBe(expected);
     });
-    test('非法地址：白名单为空（return ""', () => {
-      [
-        {
-          address: '北京市北京市大兴区行远街道101号',
-          title: '大兴星巴克',
-          expected: '',
-        },
-        {
-          address: '上海市普陀区曹杨路1040弄1-2号',
-          title: '(普陀)伟龙商务楼',
-          expected: '',
-        },
-        {
-          address: '重庆市渝北区洪湖东路1号',
-          title: '(渝北区)财富MALL',
-          cityWhiteList: [''],
-          expected: '',
-        },
-        {
-          address: '广东省深圳市福田区莲花街道红荔路6030号',
-          title: '(福田区)莲花山公园',
-          cityWhiteList: [''],
-          expected: '',
-        },
-      ].forEach(({ address, title, cityWhiteList, expected }) => {
-        const area = getArea({ address, title, cityWhiteList });
-        expect(area).toBe(expected);
-      });
+  });
+  test('非法地址：白名单为空（return ""', () => {
+    [
+      {
+        address: '北京市北京市大兴区行远街道101号',
+        title: '大兴星巴克',
+        expected: '',
+      },
+      {
+        address: '上海市普陀区曹杨路1040弄1-2号',
+        title: '(普陀)伟龙商务楼',
+        expected: '',
+      },
+      {
+        address: '重庆市渝北区洪湖东路1号',
+        title: '(渝北区)财富MALL',
+        cityWhiteList: [''],
+        expected: '',
+      },
+      {
+        address: '广东省深圳市福田区莲花街道红荔路6030号',
+        title: '(福田区)莲花山公园',
+        cityWhiteList: [''],
+        expected: '',
+      },
+    ].forEach(({ address, title, cityWhiteList, expected }) => {
+      const area = getArea({ address, title, cityWhiteList });
+      expect(area).toBe(expected);
     });
-    test('非法地址：address为空（return ""）', () => {
-      [
-        {
-          address: '',
-          title: '大兴星巴克',
-          expected: '',
-          cityWhiteList: ['北京市'],
-        },
-        {
-          address: '-2号',
-          title: '(普陀)伟龙商务楼',
-          expected: '',
-          cityWhiteList: ['北京市'],
-        },
-        {
-          address: '',
-          title: '(渝北区)财富MALL',
-          cityWhiteList: [''],
-          expected: '',
-        },
-        {
-          address: '',
-          title: '(福田区)莲花山公园',
-          cityWhiteList: [''],
-          expected: '',
-        },
-      ].forEach(({ address, title, cityWhiteList, expected }) => {
-        const area = getArea({ address, title, cityWhiteList });
-        expect(area).toBe(expected);
-      });
+  });
+  test('非法地址：address为空（return ""）', () => {
+    [
+      {
+        address: '',
+        title: '大兴星巴克',
+        expected: '',
+        cityWhiteList: ['北京市'],
+      },
+      {
+        address: '-2号',
+        title: '(普陀)伟龙商务楼',
+        expected: '',
+        cityWhiteList: ['北京市'],
+      },
+      {
+        address: '',
+        title: '(渝北区)财富MALL',
+        cityWhiteList: [''],
+        expected: '',
+      },
+      {
+        address: '',
+        title: '(福田区)莲花山公园',
+        cityWhiteList: [''],
+        expected: '',
+      },
+    ].forEach(({ address, title, cityWhiteList, expected }) => {
+      const area = getArea({ address, title, cityWhiteList });
+      expect(area).toBe(expected);
     });
   });
 });
