06f5b2b42915722af81a4dfe2c52e4a6c85da950
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2024-05-11 17:17:02 +0800

    fix: small fix

--- src/misc/get-area.ts
+++ src/misc/get-area.ts
@@ -26,18 +26,14 @@ export const getArea = ({
 };
 
 function addressNormalizer(address: string) {
+  // 今后更多的地址清洗规则可以在这里添加
   const parenthesisRegex = /[\(\（][^()\（\）]*[\)\）]|[\[\【][^[\]\】\[\【]*[\]\】]/g;
-  return address.replace(parenthesisRegex, '');
+  const afterStreetTextRegex = /((?:街|道|路)\d+号).*/;
+  return address.replace(parenthesisRegex, '').replace(afterStreetTextRegex, '$1');
 }
 
-function regexMode(address: string) {
-  if (
-    /(?:街|道|路)\d+号(?<city>[^自治州]+自治州|[^特别行政区]+特别行政区|[^市]+市|.*?地区|.*?行政单位|.+盟|市辖区|[^县]+县)/.test(
-      address,
-    )
-  ) {
-    return /(?<province>[^省]+省|.+自治区|[^澳门]+澳门|[^香港]+香港|[^市]+市)?(?<city>[^自治州]+自治州|[^特别行政区]+特别行政区|[^市]+市|.*?地区|.*?行政单位|.+盟|市辖区|[^县]+县)(?<district>[^县]+县|[^镇]+镇|[^区]+区|[^乡]+乡|.+场|.+旗|.+海域|.+岛)?(?<address>.*)/;
-  }
+function regexMode(_: string) {
+  // 今后更多的正则表达式模式可以在这里添加
   return /(?<province>[^省]+省|.+自治区|[^澳门]+澳门|[^香港]+香港|[^市]+市)?(?<city>[^自治州]+自治州|[^特别行政区]+特别行政区|[^市]+市|.*?地区|.*?行政单位|.+盟|市辖区|[^县]+县)(?<district>[^县]+县|[^市]+市|[^镇]+镇|[^区]+区|[^乡]+乡|.+场|.+旗|.+海域|.+岛)?(?<address>.*)/;
 }
 
@@ -70,3 +66,12 @@ function isValidDistrict({
   const { province, city } = locationGroups;
   return cityWhiteList.some((c) => province?.includes(c) || city?.includes(c));
 }
+import { a } from './a';
+const b = a.map((x) => {
+  return {
+    ...x,
+    getAreaResult: getArea(x),
+  };
+});
+const path = import.meta.dir + '/file.json';
+await Bun.write(path, JSON.stringify(b, null, 2));
