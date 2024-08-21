1984eb5a529d43da5222f9b18f027784987f51ef
Kenji Ginjo <120051456+KenjiGinjo@users.noreply.github.com>
2024-08-21 10:11:58 +0800

    fix(test): oid

--- packages/utility/src/generator/oid.spec.ts
+++ packages/utility/src/generator/oid.spec.ts
@@ -1,11 +1,16 @@
 import { describe, expect, test } from 'bun:test';
+import { format } from 'date-fns';
 import { oid } from './oid';
 
 describe('oid', () => {
   test('normal usage', () => {
-    // TODO: more test cases
+    expect(new Date('2022-01-01').toISOString().slice(2, 10).replace(/-/g, '')).toBe('220101');
+    expect(new Date('2022-12-12').toISOString().slice(2, 10).replace(/-/g, '')).toBe('221212');
+
     const _oid = oid();
 
     expect(_oid.length).toEqual(20);
+
+    expect(_oid).toStartWith(format(new Date(), 'yyMMdd'));
   });
 });
