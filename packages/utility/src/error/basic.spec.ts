599885e693698beee1dfeb4819daadd791c9908b
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2026-04-27 21:37:01 +0800

    feat(utility): add LogicError

--- packages/utility/src/error/basic.spec.ts
+++ packages/utility/src/error/basic.spec.ts
@@ -1,15 +1,33 @@
 import { describe, expect, test } from 'bun:test';
-import { AbortError, ParamError, TimeoutError } from './basic';
+import { AbortError, LogicError, ParamError, TimeoutError } from './basic';
 import { isTaggedError } from './factory/isTaggedError';
 
 describe('basic', () => {
+  test('LogicError', () => {
+    const error = new LogicError();
+
+    expect(error).toBeInstanceOf(LogicError);
+    expect(error.name).toBe('LogicError');
+    expect(error._tag).toBe('__XSTOOLS_UTILITY__LOGIC_ERROR');
+    expect(error.message).toBe('LOGIC_ERROR');
+    expect(new LogicError('xxx').message).toBe('xxx');
+
+    expect(isTaggedError(error)).toBe(true);
+    expect(isTaggedError(error, LogicError)).toBe(true);
+    expect(isTaggedError(error, Error as any)).toBe(true); // 意义不大
+    expect(error instanceof LogicError).toBe(true);
+    expect(error instanceof Error).toBe(true);
+    expect(LogicError.is(error)).toBe(true);
+    expect(LogicError.is(new Error('plain error'))).toBe(false);
+  });
+
   test('AbortError', () => {
     const error = new AbortError();
 
     expect(error).toBeInstanceOf(AbortError);
     expect(error.name).toBe('AbortError');
     expect(error._tag).toBe('__XSTOOLS_UTILITY__ABORT_ERROR');
-    expect(error.message).toBe('The operation was aborted');
+    expect(error.message).toBe('ABORT_ERROR');
     expect(new AbortError('xxx').message).toBe('xxx');
 
     expect(isTaggedError(error)).toBe(true);
@@ -18,6 +36,7 @@ describe('basic', () => {
     expect(error instanceof AbortError).toBe(true);
     expect(error instanceof Error).toBe(true);
     expect(AbortError.is(error)).toBe(true);
+    expect(AbortError.is(new Error('plain error'))).toBe(false);
   });
 
   test('ParamError', () => {
@@ -26,7 +45,7 @@ describe('basic', () => {
     expect(error).toBeInstanceOf(ParamError);
     expect(error.name).toBe('ParamError');
     expect(error._tag).toBe('__XSTOOLS_UTILITY__PARAM_ERROR');
-    expect(error.message).toBe('The parameter is invalid');
+    expect(error.message).toBe('PARAM_ERROR');
     expect(new ParamError('xxx').message).toBe('xxx');
 
     expect(isTaggedError(error)).toBe(true);
@@ -35,6 +54,7 @@ describe('basic', () => {
     expect(error instanceof ParamError).toBe(true);
     expect(error instanceof Error).toBe(true);
     expect(ParamError.is(error)).toBe(true);
+    expect(ParamError.is(new Error('plain error'))).toBe(false);
   });
 
   test('TimeoutError', () => {
@@ -43,7 +63,7 @@ describe('basic', () => {
     expect(error).toBeInstanceOf(TimeoutError);
     expect(error.name).toBe('TimeoutError');
     expect(error._tag).toBe('__XSTOOLS_UTILITY__TIMEOUT_ERROR');
-    expect(error.message).toBe('The operation was timed out');
+    expect(error.message).toBe('TIMEOUT_ERROR');
     expect(new TimeoutError('xxx').message).toBe('xxx');
 
     expect(isTaggedError(error)).toBe(true);
@@ -52,5 +72,6 @@ describe('basic', () => {
     expect(error instanceof TimeoutError).toBe(true);
     expect(error instanceof Error).toBe(true);
     expect(TimeoutError.is(error)).toBe(true);
+    expect(TimeoutError.is(new Error('plain error'))).toBe(false);
   });
 });
--- packages/utility/src/error/basic.ts
+++ packages/utility/src/error/basic.ts
@@ -1,19 +1,25 @@
 import { createTaggedError } from './factory';
 
+export class LogicError extends createTaggedError('__XSTOOLS_UTILITY__LOGIC_ERROR') {
+  public constructor(message = 'LOGIC_ERROR') {
+    super(message);
+  }
+}
+
 export class AbortError extends createTaggedError('__XSTOOLS_UTILITY__ABORT_ERROR') {
-  public constructor(message = 'The operation was aborted') {
+  public constructor(message = 'ABORT_ERROR') {
     super(message);
   }
 }
 
 export class ParamError extends createTaggedError('__XSTOOLS_UTILITY__PARAM_ERROR') {
-  public constructor(message = 'The parameter is invalid') {
+  public constructor(message = 'PARAM_ERROR') {
     super(message);
   }
 }
 
 export class TimeoutError extends createTaggedError('__XSTOOLS_UTILITY__TIMEOUT_ERROR') {
-  public constructor(message = 'The operation was timed out') {
+  public constructor(message = 'TIMEOUT_ERROR') {
     super(message);
   }
 }
