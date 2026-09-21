f17bcba74812c49f404dc5f4ee207bc5141e8ad3
Bingqing Wang <10382462+bingtsingw@users.noreply.github.com>
2026-09-21 08:53:16 +0800

    chore: vp check add ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite-plus';
 
 export default defineConfig({
   staged: {
-    '**/*.{js,jsx,ts,tsx}': 'vp check',
-    '**/*.{html,css,md,json,prisma}': 'vp fmt',
+    '**/*.{js,jsx,ts,tsx,mjs,cjs,mts,cts}': 'vp check',
+    '**/*.{html,css,scss,less,md,mdx,json,jsonc,json5,yaml,yml,toml,graphql,gql,vue}': 'vp fmt',
   },
   lint: {
+    options: {
+      typeAware: true,
+      typeCheck: true,
+    },
+    plugins: ['typescript', 'unicorn', 'oxc', 'react', 'import'],
     rules: {
       'no-unused-vars': [
         'error',
@@ -16,9 +21,33 @@ export default defineConfig({
           caughtErrorsIgnorePattern: '^_',
         },
       ],
+      'no-fallthrough': 'error',
     },
   },
   fmt: {
+    ignorePatterns: [
+      '**/.DS_Store',
+      '**/.eslintcache',
+      '**/yarn.lock',
+      '**/pnpm-lock.yaml',
+      '**/package-lock.json',
+      '**/node_modules',
+      '**/.husky',
+      '**/*.log',
+      '**/*.ico',
+      '**/*.svg',
+      '**/*.png',
+      '**/*ignore',
+      '**/.editorconfig',
+      '**/CHANGELOG.md',
+      '**/build',
+      '**/coverage',
+      '**/dist',
+      '**/.next',
+      '**/.umi',
+      '**/.umi-test',
+      '**/.umi-production',
+    ],
     printWidth: 120,
     proseWrap: 'never',
     singleQuote: true,
